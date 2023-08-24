67c2254436b452f9f4da0d64b6302d0e9a096668
joyalzzy <joyalzzy@proton.me>
2023-08-24 22:27:08 +0800

    update script

--- scripts/changelog.py
+++ scripts/changelog.py
@@ -12,24 +12,24 @@ def getdat(f: str):
                 return [tom["name"], tom["update"]["curseforge"]["project-id"]]
 
     except Exception as e:
-#        print(e)
-        return ["",""]
+        return [e,""]
 
 def getroot():
     return (subprocess.run(["git", "rev-parse", "--show-toplevel"], capture_output=True).stdout.decode('ascii').strip())
 
 def getdel(f: str):
     try: 
+        # print(f)
         tom = tomllib.loads(subprocess.run(["git", "show", f"HEAD~{sys.argv[1]}:" + f], capture_output=True).stdout.decode('ascii').strip())
         try: 
             return [tom["name"], tom["update"]["modrinth"]["mod-id"]]
         except:
             return [tom["name"], tom["update"]["curseforge"]["project-id"]] 
-    except:
-        return ["idk", "idk"]
+    except Exception as e:
+        return [e, ""]
         
 os.chdir(getroot())
 logs = subprocess.run(["git", "diff", "--name-status",  f"HEAD~{sys.argv[1]}"], capture_output=True).stdout.decode('ascii').strip()
-logs = filter(lambda x: '1.20.1/mods' in x and not x.startswith('M'), logs.split('\n'))
-changes = map(lambda x: [f"added {getdat(x[2 : None])[0]}", f"removed {getdel(x[2: None])[0]}"][np.where([str(x).startswith('A'), str(x).startswith('D')])[0][0]],logs)
+logs = filter(lambda x: '1.20.1/mods' in x, logs.split('\n'))
+changes = map(lambda y: [f"added {getdat(y[2 : None])[0]}", f"removed {getdel(y[2: None])[0]}"][np.where([y.startswith('A'),y.startswith('D')])[0][0]],logs)
 print("\n".join(list(changes)))

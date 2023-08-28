04158223693992f01eabf3791c90e62b81ea2dc6
joyalzzy <joyalzzy@proton.me>
2023-08-28 18:42:16 +0800

    testing merge

--- scripts/add.sh
+++
@@ -1,5 +0,0 @@
-while read p; do
-  echo "adding $p"
-  packwiz modrinth add -y $p 
-done < $1
-
--- scripts/changelog.py
+++ scripts/changelog.py
@@ -28,8 +28,9 @@ def getdel(f: str):
     except Exception as e:
         return [e, ""]
         
+#print(sys.argv)
 os.chdir(getroot())
 logs = subprocess.run(["git", "diff", "--name-status",  f"HEAD~{sys.argv[1]}"], capture_output=True).stdout.decode('ascii').strip()
 logs = filter(lambda x: '1.20.1/mods' in x, logs.split('\n'))
-changes = map(lambda y: [f"added {getdat(y[2 : None])[0]}", f"removed {getdel(y[2: None])[0]}"][np.where([y.startswith('A'),y.startswith('D')])[0][0]],logs)
+changes = map(lambda y: [f"added {getdat(y[2 : None])[0]}", f"removed {getdel(y[2: None])[0]}", f"modified {getdel(y[2: None])[0]}"][np.where([y.startswith('A'),y.startswith('D'),y.startswith('M')])[0][0]],logs)
 print("\n".join(list(changes)))
